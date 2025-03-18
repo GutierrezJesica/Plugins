49ad03cf8033e6053dea30cc688b6ae94b787818
Gutierrez Jesica <jesica89@gmail.com>
2025-03-18 13:41:00 -0300

    archivo css creado

---
+++ Gastos-mensuales/funciones.php
@@ -0,0 +1,3 @@
+$gastos_f = [];
+$gastos_v = [];
+
---
+++ Gastos-mensuales/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
+    <title>Programita de gastos mensuales</title>
+</head>
+<body>
+    <h1>Programita de gastos mensuales</h1>
+    <br>
+    <div id="gastos">
+        <input type="number" name="Gastos" id="gastos" placeholder="Ingrese el monto">
+    <br>
+    </div>
+        <input type="number" name="Ingresos" id="ingresos" placeholder="Ingrese el monto">
+    <br>
+    <div id="ingresos">
+    <button>Enviar</button>
+</body>
+</html>
