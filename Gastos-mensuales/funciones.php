f692a5d13dcda41982cde244460bdd1fb4ea62f8
Gutierrez Jesica <jesica89@gmail.com>
2025-03-20 11:45:45 -0300

    incorporación de fecha

--- Gastos-mensuales/funciones.php
+++ Gastos-mensuales/funciones.php
@@ -1,3 +1,9 @@
+<?php
+
 $gastos_f = [];
 $gastos_v = [];
 
+date_default_timezone_set("America/Argentina/Buenos_Aires");
+
+$dia_actual = date("l");
+$fecha_actual = date("d-m-Y");
--- Gastos-mensuales/index.php
+++ Gastos-mensuales/index.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilos.css">
-    <title>Programita de gastos mensuales</title>
+    <title>Programita</title>
 </head>
 <body>
     <div class="box">
+        <div class ="fecha">
+            <p><?php include("funciones.php");
+                echo $dia_actual . " " . $fecha_actual; ?></p>
+        </div>
         <h1>Programita de gastos mensuales</h1>
         <br>
             <div class="contenedor">
